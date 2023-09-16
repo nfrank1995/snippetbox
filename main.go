32e57d6fec669d78ae3b03a21febed8b168241fb
nfrank1995 <franknikita19@gmail.com>
2023-09-16 18:16:53 +0200

    add id parameter to snippetView handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 
@@ -17,7 +19,14 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request){
-  w.Write([]byte("Display a specific snippet..."))
+  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+  if err != nil || id < 1 {
+    http.NotFound(w,r)
+    return
+  }
+
+  fmt.Fprintf(w, "Display a specific snippet wit ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request){
