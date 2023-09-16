6572f66c9952534bcca0f611eb2f987a1c91a864
nfrank1995 <franknikita19@gmail.com>
2023-09-16 23:01:25 +0200

    introduces dependency injection for handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,13 @@ package main
 import (
     "fmt"
     "html/template"
-    "log"
     "net/http"
     "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the home handler so it is defined as a method against
+// *application.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
@@ -22,19 +23,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Print(err.Error())
+        // Because the home handler is now a method against the application
+        // struct it can access its fields, including the structured logger. We'll 
+        // use this to create a log entry at Error level containing the error
+        // message, also including the request method and URI as attributes to 
+        // assist with debugging.
+        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        log.Print(err.Error())
+        // And we also need to update the code here to use the structured logger
+        // too.
+        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetView handler so it is defined as a method
+// against *application.
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
@@ -44,7 +54,9 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetCreate handler so it is defined as a method
+// against *application.
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -53,3 +65,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
     w.Write([]byte("Create a new snippet..."))
 }
+
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
   "os"
 )
 
+type application struct {
+  logger *slog.Logger
+}
+
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
     logLevel := flag.String("log-level", "Info", "The minimum log level to log. Supported severity levels are Debug, Info, Warning and Error in that order.")
@@ -29,15 +33,20 @@ func main() {
       logger.Warn("could not set log level, fall back to log level Info", slog.String("log-level", *logLevel))
     }
 
+    
+    app := &application{
+        logger: logger,
+    }
+
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
 
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
-    mux.HandleFunc("/snippet/create", snippetCreate)
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet/view", app.snippetView)
+    mux.HandleFunc("/snippet/create", app.snippetCreate)
 
     logger.Info("starting server", "addr", *addr)
 
