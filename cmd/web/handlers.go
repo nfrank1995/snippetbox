72672564f2e7da913aa85c74568eb2ebf2999a21
nfrank1995 <franknikita19@gmail.com>
2023-09-17 19:20:48 +0200

    adds helper methods for error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,11 +7,9 @@ import (
     "strconv"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -23,43 +21,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        // Because the home handler is now a method against the application
-        // struct it can access its fields, including the structured logger. We'll 
-        // use this to create a log entry at Error level containing the error
-        // message, also including the request method and URI as attributes to 
-        // assist with debugging.
-        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, r, err)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        // And we also need to update the code here to use the structured logger
-        // too.
-        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, r, err)
     }
 }
 
-// Change the signature of the snippetView handler so it is defined as a method
-// against *application.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Change the signature of the snippetCreate handler so it is defined as a method
-// against *application.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
