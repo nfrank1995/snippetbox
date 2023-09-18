c3957c60ea3ae7e9daaa95385fefbfdd0b51faac
nfrank1995 <franknikita19@gmail.com>
2023-09-18 20:40:51 +0200

    adds a database connection pool for a mysql database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+  "database/sql"
   "flag"
   "log/slog"
   "net/http"
   "os"
+
+  _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,17 +17,37 @@ type application struct {
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
     logLevel := flag.String("log-level", "Info", "The minimum log level to log. Supported severity levels are Debug, Info, Warning and Error in that order.")
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
     flag.Parse()
 
+    logger := createLogger(*logLevel)
 
-    app := &application{
-        logger: createLogger(*logLevel),
+    db, err := openDB(*dsn)
+    if err != nil {
+      logger.Error(err.Error())
     }
 
-    app.logger.Info("starting server", "addr", *addr)
+    defer db.Close()
 
-    err := http.ListenAndServe(*addr, app.routes())
-    app.logger.Error(err.Error())
+    app := &application{
+        logger: logger,
+    }
+
+    logger.Info("starting server", "addr", *addr)
+
+    err = http.ListenAndServe(*addr, app.routes())
+    logger.Error(err.Error())
     os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+    return nil, err
+  }
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
 }
