90cc067dc10121d54edc2363f6719c76f4c1981d
nfrank1995 <franknikita19@gmail.com>
2023-11-03 20:25:00 +0100

    adds a create html form for sippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,67 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "strconv"
-  "github.com/nfrank1995/snippetbox/internal/models"
-  "errors"
-  "github.com/julienschmidt/httprouter"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/nfrank1995/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-  snippets, err := app.snippets.Latest()
-  if err != nil {
-    app.serverError(w, r, err)
-    return
-  }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippets = snippets
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-  app.render(w, r, http.StatusOK, "home.tmpl", data)
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  params := httprouter.ParamsFromContext(r.Context())
-  
-  id, err := strconv.Atoi(params.ByName("id"))
-  if err != nil || id < 1 {
-    app.notFound(w)
-    return
-  }
+	params := httprouter.ParamsFromContext(r.Context())
 
-  snippet, err := app.snippets.Get(id)
-  if err != nil {
-    if errors.Is(err, models.ErrNoRecord) {
-      app.notFound(w)
-    } else {
-      app.serverError(w, r, err)
-    }
-    return
-  }
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
 
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippet = snippet
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-  app.render(w, r, http.StatusOK, "view.tmpl", data)
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  title := "O snail"
-  content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-  expires := 7
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-  id, err := app.snippets.Insert(title, content, expires)
-  if err != nil {
-    app.serverError(w, r, err)
-    return
-  }
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,65 +1,70 @@
 package main
 
 import (
-  "database/sql"
-  "flag"
-  "html/template"
-  "log/slog"
-  "net/http"
-  "os"
-  "github.com/nfrank1995/snippetbox/internal/models"
+	"database/sql"
+	"flag"
+	"html/template"
+	"log/slog"
+	"net/http"
+	"os"
 
-  _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/nfrank1995/snippetbox/internal/models"
 )
 
 type application struct {
-  logger *slog.Logger
-  snippets *models.SnippetModel
-  templateCache map[string]*template.Template
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
-    logLevel := flag.String("log-level", "Info", "The minimum log level to log. Supported severity levels are Debug, Info, Warning and Error in that order.")
-    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	logLevel := flag.String(
+		"log-level",
+		"Info",
+		"The minimum log level to log. Supported severity levels are Debug, Info, Warning and Error in that order.",
+	)
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
-    flag.Parse()
+	flag.Parse()
 
-    logger := createLogger(*logLevel)
+	logger := createLogger(*logLevel)
 
-    db, err := openDB(*dsn)
-    if err != nil {
-      logger.Error(err.Error())
-    }
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+	}
 
-    defer db.Close()
+	defer db.Close()
 
-    templateCache, err := newTemplateCache()
-    if err != nil {
-      logger.Error(err.Error())
-      os.Exit(1)
-    }
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
-    app := &application{
-        logger: logger,
-        snippets: &models.SnippetModel{DB: db},
-        templateCache: templateCache,
-    }
+	app := &application{
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
 
-    logger.Info("starting server", "addr", *addr)
+	logger.Info("starting server", "addr", *addr)
 
-    err = http.ListenAndServe(*addr, app.routes())
-    logger.Error(err.Error())
-    os.Exit(1)
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
-  db, err := sql.Open("mysql", dsn)
-  if err != nil {
-    return nil, err
-  }
-  if err = db.Ping(); err != nil {
-    return nil, err
-  }
-  return db, nil
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
